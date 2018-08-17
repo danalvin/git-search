3efb580dce411ffe14576233ce2438929e14bd61
danalvin <mungaiwaituika@gmail.com>
2018-08-17 17:35:50 +0300

    content added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ProfileService } from './profile.service';
+import { HttpModule } from '@angular/http';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ ProfileService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
